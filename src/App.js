d3d6cef67d286829bb5e728ffbe4ca2a02a9c7a4
Kelvin Chao <kelvinchao2011@gmail.com>
2022-06-25 21:53:49 -0700

    cleaned up components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navigation from './components/Nav/index'
 import { Home } from "./Home";
-import { About } from "./About";
-import { Projects } from "./Projects";
-import { Contact } from "./Contact";
-import { NoMatch } from "./NoMatch";
-import { Layout } from "./components/Layout/index.js";
+import { About } from "./components/About/index";
+import { Projects } from "./components/Projects/index";
+import { Contact } from "./components/Contact/index";
+import { NoMatch } from "./components/NoMatch/index";
+import { Layout } from "./components/Layout/index";
 
 
 function App() {
