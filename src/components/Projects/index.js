5c37a69ccf60e215994a655aa1c1f518297d4f0b
Kelvin Chao <kelvinchao2011@gmail.com>
2022-06-26 22:44:10 -0700

    working on dynamic cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,33 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Card, Button } from "react-bootstrap";
 
 // auto card creater take in props
 // Work in progress
-const ProjectCard = (props) => {
-  // const [] = useState([])
+// function Projects() {
+//   const project = [
+//     {
+//       name: 'Note Taker App',
+//       image: './my-portfolio/src/assets/images/ProjectImages/NoteTakerApp.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://warm-dusk-77651.herokuapp.com/',
+//     },
+//     {
+//       name: 'Budget Tracker App',
+//       image: './my-portfolio/src/assets/images/ProjectImages/BudgetApp.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://arcane-sands-29881.herokuapp.com/',
+//     },
+//     {
+//       name: 'Weather Dashboard',
+//       image: './my-portfolio/src/assets/images/ProjectImages/WeatherApp.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://kkwchao.github.io/Weather-Dashboard/',
+//     },
+//     {
+//       name: 'Team Profile Generator',
+//       image: './my-portfolio/src/assets/images/ProjectImages/TeamProfile.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://github.com/KKWChao/team-profile-generator',
+//     },
+//     {
+//       name: 'Task Planner',
+//       image: './my-portfolio/src/assets/images/ProjectImages/TaskPlanner.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://kkwchao.github.io/Task-Planner/',
+//     },
+//     {
+//       name: 'Quiz App',
+//       image: './my-portfolio/src/assets/images/ProjectImages/QuizApp.png',
+//       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+//       link: 'https://kkwchao.github.io/Quiz-App/',
+//     },
+//   ]
 
-  return (
-    <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                1 This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  )
-}
+//   const renderCard = (card, index) => {
+//     return (
+//       <Col className="m-2">
+//         <Card key={index}>
+//         <Card.Img variant="top" src={card.image} />
+//         <Card.Body className="text-center">
+//           <Card.Title>{card.name}</Card.Title>
+//           <Card.Text>
+//             {card.text}
+//           </Card.Text>
+//           <Button variant="primary" className="buttonPrimary">
+//             <a 
+//               className="noDec" 
+//               href={card.link}
+//               target="_blank"
+//               rel="noopener noreferrer">
+//                 Go to App!
+//             </a>
+//           </Button>
+//         </Card.Body>
+//       </Card>
+//     </Col>
+// )
+//   }
+
+//   return (
+//     <div className="projectCards">
+//       <section className="m-2 p-2 d-flex flex-column min-vh-100">
+//         <Row>
+//           {project.map(renderCard)}
+//         </Row>
+//       </section>
+//     </div>
+//   )
+
+// /*     <Row xs={1} md={2} className="g-4">
+//     {Array.from({ length: 6 }).map((_, idx) => (
+//       <Col>
+//         <Card>
+//           <Card.Img variant="top" src="holder.js/100px180" />
+//           <Card.Body>
+//             <Card.Title>Card title</Card.Title>
+//             <Card.Text>
+//               1 This is a longer card with supporting text below as a natural
+//               lead-in to additional content. This content is a little bit
+//               longer.
+//             </Card.Text>
+//           </Card.Body>
+//         </Card>
+//       </Col>
+//     ))}
+//   </Row> */
+// }
+
+// export default Projects()
 
 
+// Temp setup for project. Converting with props 
 export const Projects = () => (
   <section className="m-2 p-2 d-flex flex-column min-vh-100">
     <h3>Projects</h3>
@@ -35,7 +108,7 @@ export const Projects = () => (
     <Row>
       <Col className="m-2">
         <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="logo512.png"></Card.Img>
           <Card.Body className="text-center">
             <Card.Title>Note Taker App</Card.Title>
             <Card.Text>
@@ -57,7 +130,7 @@ export const Projects = () => (
 
       <Col className="m-2">
         <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="logo512.png" />
           <Card.Body className="text-center">
             <Card.Title>Budget Tracker</Card.Title>
             <Card.Text>
@@ -79,7 +152,7 @@ export const Projects = () => (
 
       <Col className="m-2">
         <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="logo512.png" />
           <Card.Body className="text-center">
             <Card.Title>Budget Tracker</Card.Title>
             <Card.Text>
@@ -148,7 +221,7 @@ export const Projects = () => (
       
       <Col className="m-2">
         <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="logo512.png" />
           <Card.Body className="text-center">
             <Card.Title>Budget Tracker</Card.Title>
             <Card.Text>
