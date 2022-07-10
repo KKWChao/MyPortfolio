a0b31a9f162a2aac138bf92c5d42f8798608bde7
Kelvin Chao <kelvinchao2011@gmail.com>
2022-07-10 15:18:47 -0700

    updated images and projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,40 +4,40 @@ import { Col, Card, Button, CardGroup } from "react-bootstrap";
 function Projects() {
   const project = [
     {
+      name: 'Cal Zone',
+      image: './ProjectImages/CalZone.png',
+      text: 'A website for you to track what you eat, and discover new and exciting cuisine.',
+      link: 'https://the-cal-zone.herokuapp.com/',
+    },
+    {
       name: 'Note Taker App',
-      image: './assets/images/ProjectImages/NoteTaker.png',
+      image: './ProjectImages/NoteTakerApp.png',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       link: 'https://warm-dusk-77651.herokuapp.com/',
     },
     {
       name: 'Budget Tracker App',
-      image: './assets/images/ProjectImages/BudgetApp.png',
+      image: './ProjectImages/BudgetApp.png',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       link: 'https://arcane-sands-29881.herokuapp.com/',
     },
     {
       name: 'Weather Dashboard',
-      image: './assets/images/ProjectImages/WeatherApp.png',
+      image: './ProjectImages/WeatherApp.png',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       link: 'https://kkwchao.github.io/Weather-Dashboard/',
     },
     {
       name: 'Team Profile Generator',
-      image: './assets/images/ProjectImages/TeamProfile.png',
+      image: './ProjectImages/TeamProfileGeneratorOutput.png',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       link: 'https://github.com/KKWChao/team-profile-generator',
     },
     {
       name: 'Task Planner',
-      image: './src/assets/images/ProjectImages/TaskPlanner.png',
+      image: './ProjectImages/TaskPlanner.png',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       link: 'https://kkwchao.github.io/Task-Planner/',
-    },
-    {
-      name: 'Quiz App',
-      image: './src/assets/images/ProjectImages/QuizApp.png',
-      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-      link: 'https://kkwchao.github.io/Quiz-App/',
     },
   ]
 
@@ -45,7 +45,7 @@ function Projects() {
     return (
       <Col className="m-2">
         <Card key={index} style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={card.image} />
+        <Card.Img variant="top" className="cardImage" src={card.image} />
         <Card.Body className="text-center">
           <Card.Title>{card.name}</Card.Title>
           <Card.Text>
